5978ceaba9b24d4c625a511030eb6346722e187d
Notonyus_risk <antoniojoserudassanchez@gmail.com>
2023-02-10 18:51:04 -0500

    update

--- red.py
+++ red.py
@@ -6,4 +6,5 @@ class capa():
     def __init__(self, n_neuronas, n_neuronas_ant, funcion_act):
         self.funcion_act = funcion_act
         self.b = np.round(stats.truncnorm.rvs(-1, 1, loc=0, scale=1, size=n_neuronas).reshape(1, n_neuronas), 3)
-    
+        self.w = np.round(stats.truncnorm.rvs(-1, 1, loc=0, scale=1, size=n_neuronas * n_neuronas_ant).reshape(n_neuronas_ant, n_neuronas), 3)
+        
